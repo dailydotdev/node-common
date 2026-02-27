57fc88f547bd54c024fdb9f00a8ccb16f162915b
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-02-27 20:26:46 +0100

    fix: packaging

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/*.ts', 'src/*/*.ts', 'src/*/*/*.ts'],
+  entry: ['src/*.ts', 'src/**/*.ts', '!src/types'],
   format: ['esm', 'cjs'],
+  treeshake: true,
   dts: true,
   clean: true,
   target: 'node22',
